5a0d8304f383420da91fe58ca0f9ee0377e238a5
pashapuma <pashapuma1@gmail.com>
2021-10-21 11:27:07 +0300

    Add files via upload

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,22 @@
+@file:Suppress("unused")
+
+object Versions {
+    // Plugins
+    const val gradle = "7.0.2"
+    const val kotlin = "1.5.21"
+
+
+    // OneSignal
+    const val oneSignalPlugin = "0.13.4"
+    const val oneSignal = "4.4.2"
+
+    // App
+    const val minSdk = 21
+    const val targetSdk = 30
+    const val compileSdk = 31
+    const val buildTools = "30.0.2"
+
+    // Frames
+    const val frames = "3.4.4"
+    const val fabMenu = "1.1.4"
+}
